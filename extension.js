ac95bea67b51549461c9a5d355b26e4d9bce266c
Cihat KOÇAK <cihat.kocak@kodris.com>
2024-02-12 23:25:24 +0300

    messagesAdded and Configrated - configAdded

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,32 +8,140 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+const settings = require("./baliconfig.json");
+const startTexts = require("./sentences/texts.json");
+const motivationsTexts = require("./sentences/motivation.json");
+const deletingTexts = require("./sentences/deletingCode.json");
+const homorTexts = require("./sentences/humor.json");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function activate(context) {
+  console.log('Congratulations, your extension "motivation" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "lovebali" is now active!');
+  let disposableStart = vscode.commands.registerCommand(
+    "loveBali.start",
+    function () {
+      vscode.window
+        .showInformationMessage(
+          startTexts[settings.language]["startText"],
+          startTexts[settings.language]["letsStart"]
+        )
+        .then((selection) => {
+          if (selection === startTexts[settings.language]["letsStart"]) {
+            vscode.commands.executeCommand("open.more.info");
+          }
+        });
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('lovebali.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+  let disposableMoreInfo = vscode.commands.registerCommand(
+    "open.more.info",
+    function () {
+      // import moreInfo.json file
+      const moreInfoTexts = require("./sentences/texts.json");
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from loveBali!');
+      vscode.window
+        .showInformationMessage(
+          moreInfoTexts[settings.language]["moreInfoText"],
+          moreInfoTexts[settings.language]["letsGo"]
+        )
+        .then((selection) => {
+          if (selection === moreInfoTexts[settings.language]["letsGo"]) {
+            vscode.commands.executeCommand("loveBali.end");
+          }
+        });
+    }
+  );
 
+  let disposableEnd = vscode.commands.registerCommand(
+    "loveBali.end",
+    function () {
+      // import end.json file
+      const endTexts = require("./sentences/texts.json");
 
+      vscode.window.showInformationMessage(
+        endTexts[settings.language]["endText"]
+      );
+    }
+  );
 
-	});
+  let timer_Thinker;
+  let timer2_AfkControl;
+  let timer3_Humor;
+  let backspaceCounter = 0;
 
-	context.subscriptions.push(disposable);
+  // Listen for text changes
+  vscode.workspace.onDidChangeTextDocument(() => {
+    console.log("Text changed!");
+    // Reset the timer every time text changes
+    clearTimeout(timer_Thinker);
+    clearTimeout(timer2_AfkControl);
+    // Start the timer for 2 minutes
+    timer_Thinker = setTimeout(() => {
+      // get last object key
+      let motivationsTextsLength = Object.keys(
+        motivationsTexts[settings.language]
+      ).length;
+      vscode.window.showInformationMessage(
+        motivationsTexts[settings.language][
+          getRandomInt(motivationsTextsLength)
+        ]
+      );
+    }, settings["noCodingMessageTimeMin"] * 60 * 1000);
+
+    timer2_AfkControl = setTimeout(() => {
+      vscode.window.showInformationMessage(
+        startTexts[settings.language]["afk"]
+      );
+    }, settings["noCodingMessageTimeMax"] * 60 * 1000);
+  });
+
+  // Listen for backspace key
+  vscode.workspace.onDidChangeTextDocument((event) => {
+    if (event.contentChanges[0].text === "") {
+      // get last object key
+      backspaceCounter++;
+      if (backspaceCounter > settings["deletingCodeMessageTime"]) {
+        let deletingTextsLength = Object.keys(
+          deletingTexts[settings.language]
+        ).length;
+        let text =
+          deletingTexts[settings.language][getRandomInt(deletingTextsLength)];
+        console.log(text);
+        vscode.window.showInformationMessage(text);
+      }
+	  backspaceCounter = 0;
+    }
+  });
+
+  timer3_Humor = setTimeout(() => {
+	let humorTextsLength = Object.keys(homorTexts[settings.language]).length;
+	vscode.window.showInformationMessage(
+	  homorTexts[settings.language][getRandomInt(humorTextsLength)]
+	);
+  }, settings["humorMessageTime"] * 60 * 1000);
+
+  context.subscriptions.push(disposableStart);
+  context.subscriptions.push(disposableMoreInfo);
+  context.subscriptions.push(disposableEnd);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+  console.log("Your extension 'motivation' is now deactivated!");
+
+  //say goodbye
+  const goodbyeTexts = require("./sentences/texts.json");
+  vscode.window.showInformationMessage(
+    goodbyeTexts[settings.language]["goodbyeText"]
+  );
+}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
